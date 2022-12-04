f929b3d6a245fb5f417b4f7f034b533ab15cd033
dimasikBurdin <mityabur@gmail.com>
2022-12-04 15:54:49 +0500

    Верстка компонента: создание нового курса/модуля

---
+++ frontend/src/app/create-container/create-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-container',
+  templateUrl: './create-container.component.html',
+  styleUrls: ['./create-container.component.scss']
+})
+export class CreateContainerComponent implements OnInit {
+  @Input() typeContainerName: 'курса' | 'модуля' = 'курса' // 'курса' || 'модуля'
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

8e327f353dbb69a6e779f86f13e48e4b3db7553f
dimasikBurdin <mityabur@gmail.com>
2022-12-07 19:18:44 +0500

    Сборка страницы: начальная страница

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 const routes: Routes = [
-  
+  {path: '', component: StartPageComponent,},
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateContainerComponent } from './components/create-container/create-container.component';
 import { LessonTypeComponent } from './components/lesson-type/lesson-type.component';
-import { CoursePreviewContainerComponent } from './components/course-preview-container/course-preview-container.component';
 import { BottomEditContainerComponent } from './components/bottom-edit-container/bottom-edit-container.component';
 import { LessonNameContainerComponent } from './components/lesson-name-container/lesson-name-container.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TopToolarContainerComponent } from './components/top-toolar-container/top-toolar-container.component';
 import { CourseMenuModule } from './components/course-menu/course-menu.module';
 import { CourseslistModule } from './components/coursesList/courseslist.module';
+import { StartPageModule } from './pages/start-page/start-page.module';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,6 @@ import { CourseslistModule } from './components/coursesList/courseslist.module';
     CreateContainerComponent,
     TopToolarContainerComponent,
     LessonTypeComponent,
-    CoursePreviewContainerComponent,
     BottomEditContainerComponent,
     LessonNameContainerComponent,
   ],
@@ -29,7 +29,8 @@ import { CourseslistModule } from './components/coursesList/courseslist.module';
     AppRoutingModule,
     FormsModule,
     CourseslistModule,
-    CourseMenuModule
+    CourseMenuModule,
+    StartPageModule
   ],
   providers: [],
   bootstrap: [AppComponent, HeaderComponent, CourseMenuModule],

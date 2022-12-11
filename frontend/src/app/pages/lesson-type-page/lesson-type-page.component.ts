9d2484b709f7763c077a6226d91ce58f5e00429e
dimasikBurdin <mityabur@gmail.com>
2022-12-11 16:49:29 +0500

    добавлен тестовый роутин + minor fix

--- frontend/src/app/pages/lesson-type-page/lesson-type-page.component.ts
+++ frontend/src/app/pages/lesson-type-page/lesson-type-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lesson-type-page',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonTypePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToCreateTextLesson(): void {
+    this.router.navigateByUrl('/edit-course/edit-text-lesson');
+  }
+
+  navigateToCreateTestLesson(): void {
+    this.router.navigateByUrl('/edit-course/edit-test-lesson');
   }
 
 }

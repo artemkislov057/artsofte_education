4fd4868cd2776742ef3644bbd133914db5d957b1
a.kislov <a.kislov@skbkontur.ru>
2022-12-14 12:49:40 +0500

    сделал миграцию после изменения нейминга

---
+++ backend/Education.DataBase/Migrations/20221214074917_change_naming_modules_lessons.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Education.DataBase.Migrations
+{
+    public partial class change_naming_modules_lessons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LiteratureElement_LiteratureWidgets_LiteratureWidgetId",
+                table: "LiteratureElement");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PresentationSlides_PresentationWidgets_PresentationId",
+                table: "PresentationSlides");
+
+            migrationBuilder.DropTable(
+                name: "LiteratureWidgets");
+
+            migrationBuilder.DropTable(
+                name: "PresentationWidgets");
+
+            migrationBuilder.DropTable(
+                name: "VideoWidgets");
+
+            migrationBuilder.DropTable(
+                name: "Widgets");
+
+            migrationBuilder.DropTable(
+                name: "Chapters");
+
+            migrationBuilder.RenameColumn(
+                name: "LiteratureWidgetId",
+                table: "LiteratureElement",
+                newName: "LiteratureLessonId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LiteratureElement_LiteratureWidgetId",
+                table: "LiteratureElement",
+                newName: "IX_LiteratureElement_LiteratureLessonId");
+
+            migrationBuilder.CreateTable(
+                name: "Modules",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modules_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lessons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    ModuleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lessons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lessons_Modules_ModuleId",
+                        column: x => x.ModuleId,
+                        principalTable: "Modules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LiteratureLessons",
+                columns: table => new
+                {
+                    LessonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LiteratureLessons", x => x.LessonId);
+                    table.ForeignKey(
+                        name: "FK_LiteratureLessons_Lessons_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lessons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PresentationLessons",
+                columns: table => new
+                {
+                    LessonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PresentationLessons", x => x.LessonId);
+                    table.ForeignKey(
+                        name: "FK_PresentationLessons_Lessons_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lessons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VideoLessons",
+                columns: table => new
+                {
+                    LessonId = table.Column<int>(type: "int", nullable: false),
+                    VideoType = table.Column<int>(type: "int", nullable: false),
+                    Src = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoLessons", x => x.LessonId);
+                    table.ForeignKey(
+                        name: "FK_VideoLessons_Lessons_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lessons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_ModuleId",
+                table: "Lessons",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_CourseId",
+                table: "Modules",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LiteratureElement_LiteratureLessons_LiteratureLessonId",
+                table: "LiteratureElement",
+                column: "LiteratureLessonId",
+                principalTable: "LiteratureLessons",
+                principalColumn: "LessonId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PresentationSlides_PresentationLessons_PresentationId",
+                table: "PresentationSlides",
+                column: "PresentationId",
+                principalTable: "PresentationLessons",
+                principalColumn: "LessonId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LiteratureElement_LiteratureLessons_LiteratureLessonId",
+                table: "LiteratureElement");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PresentationSlides_PresentationLessons_PresentationId",
+                table: "PresentationSlides");
+
+            migrationBuilder.DropTable(
+                name: "LiteratureLessons");
+
+            migrationBuilder.DropTable(
+                name: "PresentationLessons");
+
+            migrationBuilder.DropTable(
+                name: "VideoLessons");
+
+            migrationBuilder.DropTable(
+                name: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Modules");
+
+            migrationBuilder.RenameColumn(
+                name: "LiteratureLessonId",
+                table: "LiteratureElement",
+                newName: "LiteratureWidgetId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LiteratureElement_LiteratureLessonId",
+                table: "LiteratureElement",
+                newName: "IX_LiteratureElement_LiteratureWidgetId");
+
+            migrationBuilder.CreateTable(
+                name: "Chapters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chapters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chapters_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Widgets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChapterId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Widgets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Widgets_Chapters_ChapterId",
+                        column: x => x.ChapterId,
+                        principalTable: "Chapters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LiteratureWidgets",
+                columns: table => new
+                {
+                    WidgetId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LiteratureWidgets", x => x.WidgetId);
+                    table.ForeignKey(
+                        name: "FK_LiteratureWidgets_Widgets_WidgetId",
+                        column: x => x.WidgetId,
+                        principalTable: "Widgets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PresentationWidgets",
+                columns: table => new
+                {
+                    WidgetId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PresentationWidgets", x => x.WidgetId);
+                    table.ForeignKey(
+                        name: "FK_PresentationWidgets_Widgets_WidgetId",
+                        column: x => x.WidgetId,
+                        principalTable: "Widgets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VideoWidgets",
+                columns: table => new
+                {
+                    WidgetId = table.Column<int>(type: "int", nullable: false),
+                    Src = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VideoType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoWidgets", x => x.WidgetId);
+                    table.ForeignKey(
+                        name: "FK_VideoWidgets_Widgets_WidgetId",
+                        column: x => x.WidgetId,
+                        principalTable: "Widgets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chapters_CourseId",
+                table: "Chapters",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Widgets_ChapterId",
+                table: "Widgets",
+                column: "ChapterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LiteratureElement_LiteratureWidgets_LiteratureWidgetId",
+                table: "LiteratureElement",
+                column: "LiteratureWidgetId",
+                principalTable: "LiteratureWidgets",
+                principalColumn: "WidgetId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PresentationSlides_PresentationWidgets_PresentationId",
+                table: "PresentationSlides",
+                column: "PresentationId",
+                principalTable: "PresentationWidgets",
+                principalColumn: "WidgetId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

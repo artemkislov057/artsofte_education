59690ff216e0bb9c381d05ca5ff02741d967a8ed
dimasikBurdin <mityabur@gmail.com>
2022-12-17 17:06:53 +0500

    Api: добавлен запрос для отображения всех модулей курса в меню

--- frontend/src/app/components/course-menu/components/module-item/module-item.component.ts
+++ frontend/src/app/components/course-menu/components/module-item/module-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-module-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./module-item.component.scss']
 })
 export class ModuleItemComponent implements OnInit {
+  @Input() title: string = '';
 
   constructor() { }
 
--- frontend/src/app/pages/edit-course-page/edit-course-page.component.ts
+++ frontend/src/app/pages/edit-course-page/edit-course-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CourseType } from 'src/typings/api/courseType';
+import { Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
+import { CourseType, Module } from 'src/typings/api/courseType';
+import { CourseMenuItem } from 'src/typings/courseMenuItemType';
 
 @Component({
   selector: 'app-edit-course-page',
@@ -7,8 +10,26 @@ import { CourseType } from 'src/typings/api/courseType';
   styleUrls: ['./edit-course-page.component.scss']
 })
 export class EditCoursePageComponent implements OnInit {
-  constructor() { }
+  menuItems: Array<Module> = [];
+
+  constructor(private apiService: AppService, private router: Router) { 
+    this.getModulesList();
+  }
 
   ngOnInit(): void {
+  }
+
+  async getModulesList() {
+    const courseId = this.apiService.getCurrentCourseId();
+    const response = await fetch(`https://localhost:5001/api/courses/${courseId}/modules`, {
+      credentials: 'include',
+    })
+    const data = await response.json() as Array<Module>;
+    console.log(data)
+    this.menuItems = data;
+  }
+
+  createModule() {
+    this.router.navigateByUrl('/create-module');
   }
 }

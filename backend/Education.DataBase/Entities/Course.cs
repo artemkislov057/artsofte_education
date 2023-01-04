6b8221ab69f06f3b7223792afaa5cfb9c2c71569
a.kislov <a.kislov@skbkontur.ru>
2023-01-04 13:59:44 +0500

    сделал колонку Description необязательной в бд

--- backend/Education.DataBase/Entities/Course.cs
+++ backend/Education.DataBase/Entities/Course.cs
@@ -6,7 +6,7 @@ public class Course : IEntity<Guid>
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
+    public string? Description { get; set; } = null!;
 
     public ICollection<Module>? Modules { get; set; }
 }

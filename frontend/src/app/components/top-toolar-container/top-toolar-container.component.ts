56ce4194d1809701f6e625e0269d850e1bda539c
dimasikBurdin <mityabur@gmail.com>
2023-01-04 18:17:21 +0500

    Fix: вроде пофиксил какой то неадекватный дудос ошибок

--- frontend/src/app/components/top-toolar-container/top-toolar-container.component.ts
+++ frontend/src/app/components/top-toolar-container/top-toolar-container.component.ts
@@ -7,24 +7,35 @@ import { Lesson } from 'src/typings/api/courseType';
   styleUrls: ['./top-toolar-container.component.scss']
 })
 export class TopToolarContainerComponent implements OnInit {
-  @Input() lessons: Array<Lesson> = []; // массив id созданных уроков - мб сохранить это все в хранилище и просто потом обращаться, чтобы не срать запросами постоянно
+  @Input() moduleId: string | null = null;
+  @Input() lessons: Array<Lesson> | null = null; // массив id созданных уроков - мб сохранить это все в хранилище и просто потом обращаться, чтобы не срать запросами постоянно
   @Output() onClickLesson = new EventEmitter<number>(); // ф-ия для перехода на определенный урок с аргументом - id урока
   @Output() onClickCreateLesson = new EventEmitter(); // ф-ия для перехода на экран создания нового урока
-  isActiveId: number | null = null;
+  isActiveId: number | null = null; // мб сделать Input()
 
   constructor() { }
 
   ngOnChanges(changes: any) {
-    const lessons = changes.lessons;
-    if(lessons.previousValue.length !== lessons.currentValue.length) {
-      this.isActiveId = this.lessons[0]?.id;
+    if(!this.lessons) {
+      return;
     }
-    if(lessons.previousValue.length && lessons.currentValue.length) {
-      const prev = lessons.previousValue;
-      const curr = lessons.currentValue;
-      if(prev[0]?.id === curr[0]?.id && prev.length + 1 === curr.length) {
-        this.isActiveId = this.lessons[this.lessons.length - 1]?.id;
-      }
+    const changeModule = changes.moduleId;
+    if(changeModule && changeModule.previousValue !== changeModule.currentValue) {
+      this.isActiveId = this.lessons[0].id;
+      return;
+    }
+    const lessons = changes.lessons;
+    
+    const prev = lessons.previousValue;
+    const curr = lessons.currentValue;
+    if(prev === null) {
+      console.log('Prev нет', prev)
+      this.isActiveId = this.lessons[0].id;
+      return;
+    }
+    if(prev.length + 1 === curr.length){
+      console.log('добавился новый урок в модуль')
+      this.isActiveId = this.lessons[this.lessons.length - 1].id;
     }
   }
 
